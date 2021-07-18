28a3cd4a7fa0406f522c43798faf9be3d0e02046
jaejung <wowjd2524@gmail.com>
2021-07-18 16:12:09 +0900

    need a modify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json"
+    );
+    this.setState({movies, isLoading: false});
+
+  };
+  componentDidMount(){
+    this.getMovies();
+  }
+  render(){
+    const {isLoading, movies} = this.state;
+    return <div>{isLoading ? "Loding..." : movies.map(movie => {
+      console.log(movie);
+      return <Movie id year title summary poster />
+    })}</div>;
+  }
+
+  
+    
 }
+
 
 export default App;
